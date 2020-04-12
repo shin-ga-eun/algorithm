637438f6eca43465f1e6ea398dad66a28ee8cec3
shin-ga-eun <sge7102@naver.com>
2020-04-12 15:26:15 +0900

    네이버 핵데이 온라인코딩테스트.

--- src/naver_hackday/ex1.java
+++ src/naver_hackday/ex1.java
@@ -8,7 +8,6 @@ package naver_hackday;
     2종류 모두 재고가 있는 경우만 배송한다.
     없거나, 알지못하는 경우는 배송하지 않는다.
     1~n번 상품의 재고상태를 출력해라.
-
     입력값
     n = 7
     delivery = {{5,6,0},{1,3,1},{1,5,0},{7,6,0},{3,7,1},{2,5,0}}
@@ -22,8 +21,20 @@ public class ex1 {
         String answer = "";
         String[] result = new String[n+1];
 
-        for(int i=0; i<n+1; i++){
+        for(int i=1; i<n+1; i++){
             result[i] = "?";
+        }
+
+        //재고가 있는 상품을 먼저 추출
+        for(int i=0; i<delivery.length; i++) {
+            int first = delivery[i][0];
+            int second = delivery[i][1];
+            int isDelivery = delivery[i][2];
+
+            if(isDelivery == 1){
+                result[first] = "O";
+                result[second] = "O";
+            }
         }
 
         for(int i=0; i<delivery.length; i++){
@@ -31,44 +42,26 @@ public class ex1 {
             int second = delivery[i][1];
             int isDelivery = delivery[i][2];
 
-            if(isDelivery==1){
-                result[first] = "O";
-                result[second] = "O";
-                break;
-            }
-            else if(isDelivery==0){
-                if(result[first].equals("O")){
-                    result[second] = "X";
-                }
-                else if(result[second].equals("O")){
-                    result[first] = "X";
-                }
-                else if(result[first].equals("X") && result[second].equals("?")){
-                    result[second] = "?";
-                }
-                else if(result[second].equals("X") && result[first].equals("?")){
-                    result[first] = "?";
-                }
-                else{
-                    result[first] = "X";
-                    result[second] = "X";
-                }
 
+            if(isDelivery==0){
+                if(result[first]=="O")
+                    result[second]="X";
+                if(result[second]=="O")
+                    result[first]="X";
             }
-
         }
 
+
         for(int i=1; i<=n; i++)
-            System.out.println(result[i]);
+            answer += result[i];
 
-
+        System.out.println(answer);
         return answer;
     }
 
     public static void main(String[] args) {
         int[][] delivery = {{5,6,0},{1,3,1},{1,5,0},{7,6,0},{3,7,1},{2,5,0}};
         String solution = solution(7, delivery);
-
 
     }
 }
