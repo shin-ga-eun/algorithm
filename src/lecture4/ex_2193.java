89e2e31117021e56ead238113d2e6d5c837f30fd
shin-ga-eun <sge7102@naver.com>
2020-04-20 21:35:25 +0900

    lecture4 이친수문제.

--- src/lecture4/ex_2193.java
+++ src/lecture4/ex_2193.java
@@ -14,18 +14,17 @@ public class ex_2193 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long dp[][] = new long[91][2];
+        long dp[] = new long[91];
 
         //예외처리
-        dp[1][0] = 0;
-        dp[1][1] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
 
-        for(int i=2; i<=N; i++){
-            dp[i][0] = dp[i-1][1] + dp[i-1][0];
-            dp[i][1] = dp[i-1][0];
+        for(int i=3; i<=N; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        System.out.println(dp[N][1]+dp[N][0]);
+        System.out.println(dp[N]);
 
 
 
